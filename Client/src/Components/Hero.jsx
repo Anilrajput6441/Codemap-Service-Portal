172c5922edc3100fa50706aaa1da419148f02993
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-11-17 19:08:11 +0530

    Update:Update in code and Hero Section

--- Client/src/Components/Hero.jsx
+++ Client/src/Components/Hero.jsx
@@ -35,9 +35,9 @@ const Hero = () => {
                 <div className="w-[50vw] flex flex-col gap-y-10 items-center mt-[15vmin]">
                   <div className="flex">
                     <div className="w-[90vmin] border-mainText  rounded-[4px] lg:ml-12 flex">
-                      <div className="verticalLine h-[8vmin]  w-2 bg-mainText mt-4 rounded-[3px] ml-7"></div>
+                      <div className="verticalLine h-[8vmin]  w-2 bg-mainText mt-4 rounded-[3px] ml-9"></div>
                       <div className="ml-2">
-                        <p className="HeroheaderText font-bold text-[8vmin] ml-2">
+                        <p className="HeroheaderText font-bold text-[8vmin] ml-3">
                           Codemap{" "}
                           <span className="text-mainText">Services</span>
                         </p>
@@ -48,7 +48,7 @@ const Hero = () => {
                     </div>
                   </div>
                   <div className="w-[90vmin] text-[2.35vmin] pl-7 pr-5 font-roboto">
-                    <p className="lg:ml-5 mt-1">
+                    <p className="lg:ml-6 mt-1">
                       We at codemap aim to deliver quality technical solutions
                       to our clients at the most affordable cost. Give us a
                       chance to make your digital dream come true!!!
