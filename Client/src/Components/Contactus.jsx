145feca60c10e43de78572db67c176d3f3bcd222
Aritra Chakraborty <aritra.chakraborty203@gmail.com>
2024-11-24 13:09:19 +0530

    Update:Solved the size problems for small screens

--- Client/src/Components/Contactus.jsx
+++ Client/src/Components/Contactus.jsx
@@ -21,7 +21,7 @@ const Contactus = () => {
     <>
       {screen.width < 700 && (
         <>
-          <div className="w-[100vw] flex flex-col pt-[9vmin] pb-[18vmin] overflow-x-hidden">
+          <div className="w-[100vw] flex flex-col pt-[9vmin] pb-[4vmin] overflow-x-hidden">
             <div className="mainHolder w-[100vw]  flex flex-col justify-center  items-center">
               <div className="textHolder w-[100vw] flex flex-col justify-center items-center">
                 <p className="text-[9vmin] font-extrabold">
@@ -69,7 +69,83 @@ const Contactus = () => {
       )}
 
       {screen.width >= 700 && screen.width <= 1000 && (
-        <div className="w-[100vw] p-5 pb-10 pt-8 "></div>
+        <>
+          <div className="w-[100vw] p-5 pt-1 pb-[10vmin] flex pl-4">
+            <div className="leftHolder w-[55vw]  flex flex-col  ">
+              <div className="textHolder flex flex-col mt-[10vmin] flex justify-center items-center">
+                <p className="title text-deepViolet text-[2.35vmin]  font-bold">
+                  Contact Us
+                </p>
+                <p className="purpose text-[8.75vmin] font-extrabold mt-[-0.75vmin]">
+                  Get In Touch
+                </p>
+                <p className="sloganBodyLarge mt-5 w-[85%] ml-10 text-[2.15vmin] leading-[5vmin]">
+                  We’d love to hear from you! Whether you have a question,
+                  suggestion, or need support, our team is here to help. Please
+                  feel free to reach out to us through any of the following
+                  channels
+                </p>
+              </div>
+
+              {apiResponseMock.map((val, index) => (
+                <div className="iconsHolder flex flex-col gap-y-8 mt-16 ml-[12vmin]">
+                  <div className="infoHolder flex gap-x-5">
+                    <div className="imageHolder">
+                      <div className="squreHolder w-[8vmin] h-[8vmin] p-3 bg-deepViolet rounded-[0.45vmin]">
+                        <img
+                          src={val.image}
+                          alt="contactIcon"
+                          className="max-w-full max-h-full object-contain"
+                        />
+                      </div>
+                    </div>
+                    <div className="textHolder w-[38vmin]">
+                      <div className="title font-extrabold font-poppins ">
+                        <p className="text-[2.15vmin]">{val.title}</p>
+                      </div>
+                      <div className="information text-[2vmin] text-textGrey mt-1">
+                        {val.description}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="rightHolder w-[45vw]  h-[100vh] flex items-center">
+              <div className="contactHolder w-[95%] mt-[12vmin]  bg-backgroundMain  rounded-[1vmin]">
+                <div className="contactForm flex flex-col ml-8 gap-y-10 mt-[5vmin] pb-[4vmin]">
+                  <p className="text-mainText font-medium text-[2.75vmin] font-poppins">
+                    Please Fill the details...
+                  </p>
+                  <input
+                    type="text"
+                    className="p-3 w-[90%] text-[2.25vmin]"
+                    placeholder="Enter your name..."
+                  />
+                  <input
+                    type="email"
+                    className="p-3 w-[90%] text-[2.25vmin]"
+                    placeholder="Enter your email..."
+                  />
+                  <input
+                    type="phone"
+                    className="p-3 w-[90%] text-[2.25vmin]"
+                    placeholder="Enter your phone..."
+                  />
+                  <textarea
+                    className="w-[90%] p-5 pb-10 text-[2.25vmin]"
+                    placeholder="Enter your message..."
+                  />
+                  <div className="buttonHolder flex justify-center items-center">
+                    <button className="submitButton bg-deepViolet text-white  w-[35%] p-3  font-medium text-[2.25vmin] mt-5 font-poppins">
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
       )}
 
       {screen.width >= 1000 && (
