714d3f0bd32c4b9729c6f818741532f4ee8942b2
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-11-30 12:33:17 +0530

    Update:Update in the Partnership Router

--- Client/src/Components/TechStack.jsx
+++ Client/src/Components/TechStack.jsx
@@ -86,20 +86,20 @@ const TechStack = () => {
               <div className="mt-12 imageContainer flex flex-wrap justify-center items-center p-10  gap-x-20 gap-y-24">
                 {data.map((val) => (
                   <div className="imageHolder h-[20vmin]  w-[20vmin] md:h-[11vmin] md:w-[11vmin] 2xl:h-[9vmin] 2xl:w-[9vmin]">
-                    {(val.name === "PHP" && val.name === "Neo4j" && (
-                      <>
-                        <p>Hi</p>
+                    {val.name === "PHP" ||
+                      (val.name === "Neo4j" && (
+                        <>
+                          <img
+                            src={API_TEST_URL + val.image}
+                            className="max-w-full max-h-full object-contain mt-[1.95vmin]"
+                          ></img>
+                        </>
+                      )) || (
                         <img
                           src={API_TEST_URL + val.image}
-                          className="max-w-full max-h-full object-contain"
+                          className="max-w-full max-h-full object-contain "
                         ></img>
-                      </>
-                    )) || (
-                      <img
-                        src={API_TEST_URL + val.image}
-                        className="max-w-full max-h-full object-contain "
-                      ></img>
-                    )}
+                      )}
                   </div>
                 ))}
               </div>
